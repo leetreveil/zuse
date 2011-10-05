1f1f897a2c69d062220f387ecef51bc65c119a68
Lee Treveil <leetreveil@gmail.com>
2011-10-05 19:01:19 +0100

    References are now embedded within the .exe. This process is currently manual, we should automate this with a build task.

---
+++ src/Zuse/PreApp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.IO;
+using System.Globalization;
+
+namespace Zuse
+{
+    public class PreApp
+    {
+        [STAThreadAttribute]
+        public static void Main()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += OnResolveAssembly;
+            Program.Main();
+        }
+
+        private static Assembly OnResolveAssembly(object sender, ResolveEventArgs args)
+        {
+            Assembly executingAssembly = Assembly.GetExecutingAssembly();
+            AssemblyName assemblyName = new AssemblyName(args.Name);
+
+            string path = assemblyName.Name + ".dll";
+            if (assemblyName.CultureInfo.Equals(CultureInfo.InvariantCulture) == false)
+            {
+                path = String.Format(@"{0}\{1}", assemblyName.CultureInfo, path);
+
+            }
+
+            var nsPrefix = "Zuse.Resources.";
+
+            using (Stream stream = executingAssembly.GetManifestResourceStream(nsPrefix + path))
+            {
+                if (stream == null)
+                    return null;
+
+                byte[] assemblyRawBytes = new byte[stream.Length];
+                stream.Read(assemblyRawBytes, 0, assemblyRawBytes.Length);
+                return Assembly.Load(assemblyRawBytes);
+            }
+        }
+    }
+}
